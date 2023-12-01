884dce077abd17cd8e007e89953447c6fbdb1b53
Arden Ranta <arranta20@gmail.com>
2023-12-01 15:47:18 -0800

    feat/edit notes and add rounding to rating

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -2,13 +2,9 @@ import React from 'react';
 import './MovieCard.css';
 import PropTypes from 'prop-types';
 
-// card contains movie details
-// poster on one side, details on the other
-// from https://rancid-tomatillos.herokuapp.com/api/v2/movies/<id>/ need to grab key values of id, title, poster_path, release_date, overview, genres, budget, revenue, runtime, tagline, and average_rating
-//currently w/o API, in the data file we have access to id, poster_path, title, release_date, and average_rating
+// from https://rancid-tomatillos.herokuapp.com/api/v2/movies/<id>/ need to grab key values of title, poster_path, release_date, overview, genres, budget, revenue, runtime, tagline, and average_rating
 
 //USER STORY: As a user, I can click a movie, and see that movie’s details
-//onclick, display the poster matching the id of the event.target, hide all other posters, header static, button to return to all appears
 
 function MovieCard({card, goBackToMain}) {
   // when API is hooked up, will need a GET request for the details of the card with the matching id
@@ -20,7 +16,7 @@ function MovieCard({card, goBackToMain}) {
         <article className='DetailDisplay'>
           <h2 className='MovieTitle'>{title}</h2>
           <h3 className='ReleaseDate'>{release_date}</h3>
-          <h3 className='Rating'>average {average_rating}/10</h3>
+          <h3 className='Rating'>average {average_rating.toFixed(2)}/10</h3>
         </article>
       </section>
       <button className='BackButton' onClick={goBackToMain}>Return to All</button>
