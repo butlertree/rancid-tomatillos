cef7f044786902d2304f751e79b2239d1c2435bb
butlertree <140225441+butlertree@users.noreply.github.com>
2023-12-05 13:07:31 -0700

    Added api hook up in MovieCard for videos api

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,18 +1,14 @@
 
+
 import './MovieCard.css';
 import React, { useState, useEffect } from 'react';
-
 import PropTypes from 'prop-types';
-
-// from https://rancid-tomatillos.herokuapp.com/api/v2/movies/<id>/ need to grab key values of title, poster_path, release_date, overview, genres, budget, revenue, runtime, tagline, and average_rating
-
-//USER STORY: As a user, I can click a movie, and see that movie’s details
 
 function MovieCard({ card, goBackToMain }) {
   const { id } = card;
   const [additionalData, setAdditionalData] = useState(null);
-
-  const [error, setError] = useState('')
+  const [videoData, setVideoData] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     // Fetch additional data based on the ID
@@ -20,9 +16,31 @@ function MovieCard({ card, goBackToMain }) {
       .then(response => response.json())
       .then(data => setAdditionalData(data.movie))
       .catch(error => setError(error.message))
-  }, [id]); // Fetch data whenever the ID changes
+  }, [id]);
+
+  useEffect(() => {
+    // Fetch video data based on the ID
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
+      .then(response => response.json())
+      .then(data => setVideoData(data.videos))
+      .catch(error => setError(error.message))
+  }, [id]);
+
+  // Function to find the YouTube Teaser video key
+  function findTeaserVideoKey(videos) {
+    let teaserKey = null;
+    videos.forEach((video) => {
+      if (video.site === "YouTube" && video.type === "Teaser" || video.type === "Trailer") {
+        teaserKey = video.key; // Set the key of the matching video
+      }
+    });
+    return teaserKey; // Return the found key or null
+  }
 
   const { title, poster_path, release_date, overview, genres, runtime, tagline, average_rating } = additionalData || {};
+
+  const teaserVideoKey = findTeaserVideoKey(videoData);
+  const videoUrl = teaserVideoKey ? `https://www.youtube.com/embed/${teaserVideoKey}` : null;
 
   return (
     <div className='movie-card'>
@@ -37,6 +55,16 @@ function MovieCard({ card, goBackToMain }) {
         <h3 className='average-rating'>average {average_rating?.toFixed(2)}/10</h3>
         <p className='overview'>{overview}</p>
       </div>
+      {teaserVideoKey && (
+        <iframe
+          width="560"
+          height="315"
+          src={videoUrl}
+          frameBorder="0"
+          allowFullScreen
+          title="Teaser Video"
+        ></iframe>
+      )}
       <button className='back-button' onClick={goBackToMain}>
         Return to All
       </button>
@@ -46,8 +74,8 @@ function MovieCard({ card, goBackToMain }) {
 
 export default MovieCard;
 
-
 MovieCard.propTypes = {
-card: PropTypes.object.isRequired,
-goBackToMain: PropTypes.func.isRequired,
+  card: PropTypes.object.isRequired,
+  goBackToMain: PropTypes.func.isRequired,
 }
+
