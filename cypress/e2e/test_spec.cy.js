6bc40c01a7f2f16d3cb5f0954c0a362385fb55f6
Arden Ranta <arranta20@gmail.com>
2023-12-07 13:01:03 -0800

    fix/remove extra spacing and boilerplate file

--- cypress/e2e/test_spec.cy.js
+++ cypress/e2e/test_spec.cy.js
@@ -5,18 +5,14 @@ describe('API calls', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
       fixture: 'apiResponse', // Use the fixture without the file extension
     }).as('api1Request');
-
     // Visit the main page of your application
     cy.visit('/');
-
     // Wait for the API request to complete
     cy.wait('@api1Request');
-
     // Assertions to check the response data or UI elements
     cy.get('img[src="https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg"]').should('be.visible');
   });
 
-  //this is straight out of the REACT docs, I'm not sure if it needs a fixture for the error?
   it('should show error messaging to a user', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', { forceNetworkError: true }).as('error');
     //wait a second
@@ -34,16 +30,12 @@ describe('API calls', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270', {
       fixture: 'apiResponse2', // Use the fixture without the file extension
     }).as('api2Request');
-
     // Visit the specified page of your application
     cy.visit('/movie/436270');
-
     // Wait for the API request to complete
     cy.wait('@api2Request');
-
     //What you should see
     cy.get('img[src="https://image.tmdb.org/t/p/original//pFlaoHTZeyNkG83vxsAJiGzfSsa.jpg"]').should('be.visible');
-
     cy.get(".runtime").contains('125 minutes');
   })
 
@@ -59,43 +51,36 @@ describe('API calls', () => {
     cy.get('h2').should('contain.text', 'Something went wrong, please try again later!');
   });
 
-
   it('should successfully retrieve data from the endpoint with the id of the selected poster', () => {
     // Intercept the GET request to the movies API and use the fixture without the extension
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270/videos', {
       fixture: 'apiResponse3', // Use the fixture without the file extension
     }).as('api3Request');
-
     // Visit the specified page of your application
     cy.visit('/movie/436270');
-
     // Wait for the API request to complete
     cy.wait('@api3Request');
     // What you should see
     cy.get('iframe[src="https://www.youtube.com/embed/JaV7mmc9HGw"]').should('be.visible');
-
-   
   })
 
   it('should show error messaging to a user regarding endpoint id', () => {
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270', { forceNetworkError: true }).as('error');
     //Wait a second
-    cy.wait(1000)
-    cy.visit('/movie/436270')
+    cy.wait(1000);
+    cy.visit('/movie/436270');
     // assert that this request happened
     // and that it ended in an error
     cy.wait('@error');
     cy.get('h2').should('contain.text', 'Something went wrong, please try again later!');
   });
 
-
-
   describe('Load Page', () => {
     beforeEach(() => {
       //Visit main page
       cy.visit('/');
     });
-    //What you sould see
+    //What you should see
     it('should display "Rancid Tomatillos" heading', () => {
       cy.get('h1').contains('Rancid Tomatillos');
     });
@@ -113,8 +98,6 @@ describe('API calls', () => {
         cy.get('.right-container').should('exist');
       });
     });
-
-    
   });
   
   //User returns to main page//
